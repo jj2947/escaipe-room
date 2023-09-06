d528ea6a9bf77ad8d2cd7b4ce075e5f48cc70cb8
Jessica Jiang <jessicaj2947@gmail.com>
2023-09-07 11:01:28 +1200

    created hallway controller and modified app, scenemanager to include it

---
+++ src/main/java/nz/ac/auckland/se206/controllers/HallwayController.java
@@ -0,0 +1,14 @@
+package nz.ac.auckland.se206.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HallwayController {
+
+  @FXML private Label timerLabel;
+
+  /** Initializes the room view, it is called when the room loads. */
+  public void initialize() {
+    // Initialization code goes here
+  }
+}
