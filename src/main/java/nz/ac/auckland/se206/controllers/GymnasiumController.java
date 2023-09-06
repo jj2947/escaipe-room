239af2fb874a1a55f7aca5c240cd8b9486964473
Jessica Jiang <jessicaj2947@gmail.com>
2023-09-07 11:38:10 +1200

    created controller for gym scene and modified App and Scene manager to include it

---
+++ src/main/java/nz/ac/auckland/se206/controllers/GymnasiumController.java
@@ -0,0 +1,14 @@
+package nz.ac.auckland.se206.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class GymnasiumController {
+
+  @FXML private Label timerLabel;
+
+  /** Initializes the room view, it is called when the room loads. */
+  public void initialize() {
+    // Initialization code goes here
+  }
+}
