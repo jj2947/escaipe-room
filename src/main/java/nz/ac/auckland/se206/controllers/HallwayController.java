32fd0921b258051e9639bd57884af9ab3b1feb1d
Jessica Jiang <jessicaj2947@gmail.com>
2023-09-07 17:28:24 +1200

    implemented the timer in the hallway scene

--- src/main/java/nz/ac/auckland/se206/controllers/HallwayController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/HallwayController.java
@@ -1,14 +1,45 @@
 package nz.ac.auckland.se206.controllers;
 
+import javafx.application.Platform;
+import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import nz.ac.auckland.se206.GameState;
 
 public class HallwayController {
 
   @FXML private Label timerLabel;
+  private Timer timer;
 
   /** Initializes the room view, it is called when the room loads. */
   public void initialize() {
     // Initialization code goes here
+    timer = GameState.timer;
+    updateTimer();
+  }
+
+  private void updateTimer() {
+    // Update the timer label every second
+    Task<Void> updateLabelTask =
+        new Task<Void>() {
+          @Override
+          protected Void call() throws Exception {
+            while (!GameState.isTimeReached) {
+              Thread.sleep(1000); // Wait for 1 second
+              Platform.runLater(
+                  () ->
+                      timerLabel.setText(
+                          String.format(
+                              "%02d:%02d", timer.getCounter() / 60, timer.getCounter() % 60)));
+              ;
+            }
+            return null;
+          }
+        };
+
+    // Create a new thread for the update task and start it
+    Thread updateThread = new Thread(updateLabelTask);
+    updateThread.setDaemon(true);
+    updateThread.start();
   }
 }
