53e4461512d9ea33de498fd2e7dd080aa8353388
Caleb Matthew <caleb.john.matthew@gmail.com>
2023-09-08 14:02:25 +1200

    fix: small importing error

--- src/main/java/nz/ac/auckland/se206/controllers/StartScreenController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/StartScreenController.java
@@ -7,6 +7,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import nz.ac.auckland.se206.App;
+import nz.ac.auckland.se206.GameState;
 import nz.ac.auckland.se206.SceneManager;
 import nz.ac.auckland.se206.SceneManager.AppUi;
 
